b3965792bec92bcd274a2935c44e6aa7287df7a2
maxfil333_BT <maxfil333@gmail.com>
2024-08-08 16:39:26 +0300

    add logger, config

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import json
+import msvcrt
+from glob import glob
+from logger import logger
+
+config = dict()
+config['save_dir'] = '__data'
+
---
+++ logger.py
@@ -0,0 +1,26 @@
+import os
+
+
+class Logger:
+    def __init__(self):
+        self.data = []
+
+    def print(self, *args, **kwargs):
+        # Извлекаем параметры sep и end из kwargs, если они есть, или задаем значения по умолчанию
+        sep = kwargs.pop('sep', ' ')
+        end = kwargs.pop('end', '\n')
+        # Формируем сообщение
+        message = sep.join(map(str, args)) + end
+        # Выводим сообщение в консоль
+        print(message, **kwargs, end='')
+        # Сохраняем сообщение в data
+        self.data.append(message)
+
+    def save(self, log_folder):
+        # Записываем логи в файл
+        log_file = os.path.join(log_folder, 'log.log')
+        with open(log_file, 'a', encoding='utf-8') as file:
+            file.writelines(self.data)
+
+
+logger = Logger()
