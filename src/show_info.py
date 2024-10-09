f03832841af6d58afeaa32e17073ddb6bb3864e7
maxfil333_BT <maxfil333@gmail.com>
2024-10-09 12:16:05 +0300

    webhookinfo

--- src/show_info.py
+++ src/show_info.py
@@ -13,17 +13,23 @@ def print_response(response):
 
 if __name__ == "__main__":
     API_URL = config['API_URL']
-    TOKEN = config['TEST_TOKEN']
+    TOKEN = config['TOKEN']
 
-    getme_url = f'https://api.telegram.org/bot{TOKEN}/getMe'
-    get_updates_url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
+    getMe = f'https://api.telegram.org/bot{TOKEN}/getMe'
+    getUpdates = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
+    getWebhookInfo = f'https://api.telegram.org/bot{TOKEN}/getWebhookInfo'
 
     print('getMe:')
-    print(f'url: {getme_url}')
-    print_response(requests.get(getme_url))
+    print(f'url: {getMe}')
+    print_response(requests.get(getMe))
     print('-' * 50)
 
     print('getUpdates:')
-    print(f'url: {get_updates_url}')
-    print_response(requests.get(get_updates_url))
+    print(f'url: {getUpdates}')
+    print_response(requests.get(getUpdates))
+    print('-' * 50)
+
+    print('getWebhookInfo:')
+    print(f'url: {getWebhookInfo}')
+    print_response(requests.get(getWebhookInfo))
     print('-' * 50)
